f63219f9ab4fb3b4719f5a659c15f5fd8f5c4eda
Lotinex <loard969@gmail.com>
2020-08-21 00:48:28 +0900

    Updates

---
+++ precache-manifest.fea6672a4f4e6e1904e046827f53e8f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669f0746536954875953eb43d0419fe3",
+    "url": "/archive/index.html"
+  },
+  {
+    "revision": "5d1d22788bb25a26e022",
+    "url": "/archive/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a7c400de50bd4b461035",
+    "url": "/archive/static/js/2.aec7c4da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/archive/static/js/2.aec7c4da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d1d22788bb25a26e022",
+    "url": "/archive/static/js/main.4469280f.chunk.js"
+  },
+  {
+    "revision": "3437d90f55deceabfbcb",
+    "url": "/archive/static/js/runtime-main.6dd18a38.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/archive/static/media/logo.5d5d9eef.svg"
+  }
+]);
