e528407937c1fc45e3ace61535ab4f4c657c363f
Lotinex <loard969@gmail.com>
2020-08-21 01:04:25 +0900

    구성 테스트

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+class Test extends React.Component {
+  render(){
+    return (
+      <h1>asdasdasd</h1>
+    )
+  }
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route path="/" exact >
+          <App />
+        </Route>
+        <Route path="/test">
+          <Test />
+        </Route>
+      </Switch>
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
